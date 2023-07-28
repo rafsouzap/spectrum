92c0618be73fc21bcab6e6213e9e40685b3faad8
Rafael de Paula <rafsouzap@icloud.com>
2023-07-28 10:15:35 -0300

    Some improves

--- server/src/controllers/ServiceTypesController.ts
+++ server/src/controllers/ServiceTypesController.ts
@@ -3,7 +3,13 @@ import db from '../database/connection';
 
 export default class ServiceTypesController {
 	async index(request: Request, response: Response): Promise<Response> {
-		const serviceTypes = await db('service_types').select(['*']);
-		return response.send(serviceTypes);
+		try {
+			const serviceTypes = await db('service_types').select();
+			return serviceTypes.length
+				? response.status(200).json(serviceTypes)
+				: response.status(404).json({ error: 'Could not find service types' });
+		} catch (error) {
+			return response.status(500).json({ error });
+		}
 	}
 }
--- server/src/controllers/SpecialtiesController.ts
+++ server/src/controllers/SpecialtiesController.ts
@@ -3,7 +3,13 @@ import db from '../database/connection';
 
 export default class SpecialtiesController {
 	async index(request: Request, response: Response): Promise<Response> {
-		const specialties = await db('specialties').select(['*']);
-		return response.send(specialties);
+		try {
+			const specialties = await db('specialties').select();
+			return specialties.length
+				? response.status(200).json(specialties)
+				: response.status(404).json({ error: 'Could not find specialties' });
+		} catch (error) {
+			return response.status(500).json({ error });
+		}
 	}
 }
